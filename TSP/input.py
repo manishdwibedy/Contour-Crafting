0255111d3566c41819712d4ac3f7b0c4005763b1
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-22 17:10:36 -0700

    starting with getting the input. Currently, getting a hard coded input.

---
+++ TSP/input.py
@@ -0,0 +1,15 @@
+
+class Input(object):
+    def __init__(self):
+        pass
+
+    def getInput(self):
+        self.matrix = {
+            0: {0: 0,1: 3,2:12,3:6},
+            1: {0: 3,1: 0,2:5, 3:-4},
+            2: {0: 12,1: 5,2:0, 3:-4},
+            3: {0: 6,1: -4,2:-4, 3:0},
+        }
+
+    def Distance(self, from_node, to_node):
+        return self.matrix[from_node][to_node]
