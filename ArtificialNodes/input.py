2e9b34ff84dae83643f0b197739292076f0a1880
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-24 16:10:18 -0700

    Reading the data.

---
+++ ArtificialNodes/input.py
@@ -0,0 +1,12 @@
+
+class Input(object):
+    def __init__(self):
+        pass
+
+    def readFile(self):
+        '''
+        This method would read the input from a file.
+        The input would be in the form of (x,y) for every node
+        '''
+        input = []
+        self.input = input
