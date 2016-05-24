0d6062be2356df1e4721bc2c46c6883d0d781fa2
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-24 16:41:51 -0700

    Adding the node information.

--- ArtificialNodes/input.py
+++ ArtificialNodes/input.py
@@ -9,26 +9,47 @@ class Input(object):
         This method would read the input from a file.
         The input would be in the form of (x,y) for every node
         '''
-        input = []
-        input.append({
+
+        nodes = []
+        edges = []
+
+        nodes.append({
+            'id': 'A',
             'x': 0,
             'y': 0
         })
-        input.append({
+        nodes.append({
+            'id': 'B',
             'x': 0,
             'y': 2
         })
-        input.append({
+        nodes.append({
+            'id': 'C',
             'x': 2,
             'y': 2
         })
-        self.input = input
+
+        edges.append({
+            'start': 'A',
+            'end': 'B'
+        })
+        edges.append({
+            'start': 'B',
+            'end': 'C'
+        })
+        self.edges = edges
+        self.nodes = nodes
+
 
     def createGraph(self):
         G=nx.Graph()
 
-        for node in self.input:
-            G.add_node(node)
+        for node in self.nodes:
+            G.add_node(node['id'])
 
-        print G.number_of_edges()
         print G.number_of_nodes()
+
+if __name__ == '__main__':
+    Nodesinput = Input()
+    Nodesinput.readFile()
+    Nodesinput.createGraph()
