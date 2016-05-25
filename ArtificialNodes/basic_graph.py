18790d6b8cde0d90d849f33e8fdb25260ffbcbcd
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-25 07:10:24 -0700

    Moving the code for creating a graph in a seperate script.

--- ArtificialNodes/basic_graph.py
+++ ArtificialNodes/basic_graph.py
@@ -1,3 +1,23 @@
+import networkx as nx
 
 class BasicGraph(object):
-    pass
+    def __init__(self, data):
+        self.data = data
+
+    def createBasicGraph(self):
+        '''
+        Creating a basic graph, if data about the graph exists.
+        '''
+        self.graph = None
+        # If graph data exists
+        if self.data:
+            self.graph = nx.Graph()
+
+            nodes = self.data['nodes']
+            edges = self.data['edges']
+
+            for node in nodes:
+                self.graph.add_node(node['id'], X = node['X'], Y = node['Y'])
+
+            for edge in edges:
+                self.graph.add_edge(edge['start'], edge['end'], DEPOSITION_EDGE = True)
