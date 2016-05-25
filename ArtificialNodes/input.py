528d84aae8a4cc00ef9427ab90bbb258dd81a9a5
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-25 07:10:44 -0700

    Removed the code for creating a basic graph.

--- ArtificialNodes/input.py
+++ ArtificialNodes/input.py
@@ -48,23 +48,7 @@ class Input(object):
 
                 self.data = data
 
-    def createBasicGraph(self):
-        '''
-        Creating a basic graph, if data about the graph exists.
-        '''
-        self.graph = None
-        # If graph data exists
-        if self.data:
-            self.graph = nx.Graph()
 
-            nodes = self.data['nodes']
-            edges = self.data['edges']
-
-            for node in nodes:
-                self.graph.add_node(node['id'], X = node['X'], Y = node['Y'])
-
-            for edge in edges:
-                self.graph.add_edge(edge['start'], edge['end'], DEPOSITION_EDGE = True)
 
     def parseGraph(self):
         '''
