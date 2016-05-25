195e015a7b72a71b511199819be69baeafd74afa
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-25 07:23:13 -0700

    Getting other nodes as well as addition of idle edges.

--- ArtificialNodes/basic_graph.py
+++ ArtificialNodes/basic_graph.py
@@ -1,4 +1,6 @@
 import networkx as nx
+from collections import Counter
+import utility
 
 class BasicGraph(object):
     def __init__(self, data):
@@ -21,3 +23,34 @@ class BasicGraph(object):
 
             for edge in edges:
                 self.graph.add_edge(edge['start'], edge['end'], DEPOSITION_EDGE = True)
+
+    def getOtherNodes(self, node):
+        '''
+        Get all nodes other than the current node
+        :param node: the current node's ID
+        :return: a list of nodes other than the current node
+        '''
+        node_list = []
+        for node_object in self.graph.node:
+            if node != node_object:
+                node_list.append(node_object)
+
+        return node_list
+
+    def addIdleEdges(self):
+        '''
+        Adding Idle edges to the graph
+        '''
+        for start_node, edges in self.graph.edge.iteritems():
+            other_nodes = Counter(self.getOtherNodes(start_node))
+            adjacent_nodes = []
+            for end_node, weight in edges.iteritems():
+                adjacent_nodes.append(end_node)
+            adjacent_nodes = Counter(adjacent_nodes)
+
+            missing_nodes = other_nodes - adjacent_nodes
+
+            for node in missing_nodes:
+                weight = utility.getDistance(self.graph, start_node, end_node)
+                self.graph.add_edge(start_node, node, weight = weight, IDLE_EDGES = True)
+            pass
