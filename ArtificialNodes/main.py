7e5d7030a115eed677ea340df891fb4d17c7e4d5
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-30 16:55:15 -0700

    Added comments.

--- ArtificialNodes/main.py
+++ ArtificialNodes/main.py
@@ -1,12 +1,15 @@
 import input, basic_graph, add_nodes
 
 if __name__ == '__main__':
+    # Reading the graph from the input file
     graphInput = input.Input()
     graphInput.readFile('input.json')
 
+    # Construction the graph from the input
     graph = basic_graph.BasicGraph(graphInput.data)
     graph.createBasicGraph()
 
+    # Adding the extra nodes in the graph depending upon the degree of the node.
     extra_nodes = add_nodes.AddingNodes(graph)
     extra_nodes.addNodes()
     pass
