c257e6c8c5cfba4a14a7ffc451af034891e44b63
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-30 17:57:44 -0700

    Computing the angle between the edges.

--- ArtificialNodes/utility.py
+++ ArtificialNodes/utility.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def getDistance(graph, start_node, end_node):
     if start_node in graph.node and end_node in graph.node:
@@ -9,3 +10,42 @@ def getDistance(graph, start_node, end_node):
         return distance
     else:
         return -1
+
+def findAngle(graph, edge_pair):
+    '''
+    Finding the angle between the edge pair in the graph
+    :param graph:  the graph information
+    :param edge_pair: the edge pair
+    :return: the angle in radians
+    '''
+    vectors = []
+    for edge in edge_pair:
+
+        point_1 = graph.node[edge[0]]
+        point_2 = graph.node[edge[1]]
+
+        # Computing the vector
+        vector = {
+            'X' : point_1['X'] - point_2['X'],
+            'Y' : point_1['Y'] - point_2['Y'],
+        }
+
+        # Appending the vector
+        vectors.append((vector['X'], vector['Y']))
+
+    return angle_between(vectors)
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+def angle_between(vector_pair):
+    '''
+    Returns the angle in radians between the pair of two vectors ::
+    :param vector_pair:
+    :return: Angle in radians
+    '''
+    v1_u = unit_vector(vector_pair[0])
+    v2_u = unit_vector(vector_pair[1])
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
