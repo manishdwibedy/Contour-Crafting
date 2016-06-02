f24e71ba5cf6a2bc6f3b101d27c5cacab5a34062
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-01 19:51:53 -0700

    Considering the idle edge cost using the idle ratio.

--- ArtificialNodes/utility.py
+++ ArtificialNodes/utility.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from constant import IDLE_RATIO
 
 def getDistance(graph, start_node, end_node):
     if start_node in graph.node and end_node in graph.node:
@@ -7,7 +8,7 @@ def getDistance(graph, start_node, end_node):
         end_node = graph.node[end_node]
         x_diff = start_node['X'] - end_node['X']
         y_diff = start_node['Y'] - end_node['Y']
-        distance = pow(x_diff ** 2 + y_diff ** 2, 0.5)
+        distance = pow((x_diff ** 2) * IDLE_RATIO + y_diff ** 2, 0.5)
         return distance
     else:
         return -1
