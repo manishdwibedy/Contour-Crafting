f21012a90f1d47e92b727b96bcb01aec56cb2760
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-02 14:01:34 -0700

    Computation of the deposition edges.

---
+++ ArtificialNodes/deposition_cost.py
@@ -0,0 +1,37 @@
+
+class DepositionCost(object):
+    '''
+    This class would be computing the depostion cost for the deposition edges
+    and would then assign them to the deposition edges.
+
+    The deposition cost would be computed using the 5 times than maximum IDLE time in the graph.
+    '''
+
+    def __init__(self):
+        pass
+
+    def computeMaxRotationCost(self):
+        '''
+        :return: Retuning the the maximum rotation cost.
+        '''
+        pass
+
+    def computeMaxIdleCost(self):
+        '''
+        :return: Retuning the the maximum idle edge cost.
+        '''
+        pass
+
+    def comptuteDepositionCost(self):
+        '''
+        :return: Retuning the the deposition cost
+        '''
+        pass
+
+    def assignDepositionCost(self):
+        '''
+        Assigning the deposition cost to the edges
+        :return:
+        '''
+        pass
+
