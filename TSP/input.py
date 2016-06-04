64c703e770857e4f75bb9af4cf344be492cb63fd
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-03 21:58:08 -0700

    Computing the alias of the nodes and using the aliases.

--- TSP/input.py
+++ TSP/input.py
@@ -7,9 +7,13 @@ class Input(object):
 
     def transform(self,):
         # {start : [{end, weight},{end, weight},....],... }
+        self.computeNodeAlias()
+
         matrix = {}
         for start_node, edges in self.graph.edge.iteritems():
+            start_node = self.node_alias[start_node]
             for end_node, edge_data in edges.iteritems():
+                end_node = self.node_alias[end_node]
                 if start_node not in matrix:
                     matrix[start_node] = {end_node : get_edge_cost(edge_data)}
                 else:
@@ -18,6 +22,14 @@ class Input(object):
         self.size = len(matrix) + 1
 
         self.matrix = matrix
+
+    def computeNodeAlias(self):
+        self.node_alias = {}
+
+        current_node_alias = 0
+        for node in self.graph.node:
+            self.node_alias[node] = current_node_alias
+            current_node_alias += 1
 
     def Distance(self, from_node, to_node):
         return self.matrix[from_node][to_node]
