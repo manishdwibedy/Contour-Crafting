8e63bba94bb401df2782491dbd696c0117ebcecd
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-09 12:12:20 -0700

    Adding a method to merge two dictionaries.

--- common/utility.py
+++ common/utility.py
@@ -82,3 +82,9 @@ def get_edge_type_cost(edge_data):
         'type': type,
         'cost': cost
     }
+
+def merge_two_dicts(x, y):
+    '''Given two dicts, merge them into a new dict as a shallow copy.'''
+    z = x.copy()
+    z.update(y)
+    return z
