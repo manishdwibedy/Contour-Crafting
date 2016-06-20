8c096a50517a0c9585cebd5606f1f00d8def2dac
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-19 18:30:21 -0700

    Interpreted the printing steps.

--- GCode/gcode.py
+++ GCode/gcode.py
@@ -11,14 +11,18 @@ class GCode(object):
         for index, node in enumerate(self.solution['nodes']):
             print "Current Node - " + node
             position = graph.node[node]
-            print "Position - (" + str(position['X']) + "," + str(position['Y']) + ')\n'
+            print "Current Position - (" + str(position['X']) + "," + str(position['Y']) + ')'
 
             # Excluding the last node in the solution
             if index < len(self.solution['nodes']) - 1:
                 next_node = self.solution['nodes'][index+1]
 
                 position = graph.node[next_node]
-                print "Position - (" + str(position['X']) + "," + str(position['Y']) + ')\n'
+                print "Destination Position - (" + str(position['X']) + "," + str(position['Y']) + ')'
 
-                pass
+                edge_info = graph.edge[node][next_node]
 
+                if 'DEPOSITION_EDGE' in edge_info:
+                    print 'Start Printing\n'
+                elif 'IDLE_COST' in edge_info:
+                    print 'Idle Edge\n'
