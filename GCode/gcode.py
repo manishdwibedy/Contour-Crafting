feedeb87af4b0d13b2e2e692ced707a8453b98eb
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-19 17:42:27 -0700

    Now, I have the current node and the next node. Need to get the edge's info.

--- GCode/gcode.py
+++ GCode/gcode.py
@@ -8,8 +8,17 @@ class GCode(object):
         print 'Starting with GCode\n'
 
         graph = self.graph.graph
-        for node in self.solution['nodes']:
+        for index, node in enumerate(self.solution['nodes']):
             print "Current Node - " + node
             position = graph.node[node]
-            print "Position - (" + str(position['X']) + "," + str(position['Y']) + ')'
-            print ''
+            print "Position - (" + str(position['X']) + "," + str(position['Y']) + ')\n'
+
+            # Excluding the last node in the solution
+            if index < len(self.solution['nodes']) - 1:
+                next_node = self.solution['nodes'][index+1]
+
+                position = graph.node[next_node]
+                print "Position - (" + str(position['X']) + "," + str(position['Y']) + ')\n'
+
+                pass
+
